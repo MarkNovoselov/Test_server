8b3e7eef3ce5095d0e178ab546aca249fc6d74ce
MarkNovoselov <167557089+MarkNovoselov@users.noreply.github.com>
2025-05-13 18:24:24 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -35,4 +35,5 @@ def download_file(filename): # скачивание файла клиентом
     return send_file(filepath, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
