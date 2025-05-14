dd328b97a08c72ec7aa70dbf9562d2554a769490
MarkNovoselov <167557089+MarkNovoselov@users.noreply.github.com>
2025-05-14 06:43:06 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -14,15 +14,7 @@ def json_endpoint():
     data = request.get_json()
     if not data:
         return jsonify({'error': 'Нет JSON'}), 400
-    print(f"Получен JSON: {data}")
-    return jsonify({'status': 'успешно', 'received': data, 'data':os.listdir()})
-
-@app.route('/download/<filename>', methods=['GET'])
-def download_file(filename): # скачивание файла клиентом
-    filepath = os.path.join(UPLOAD_FOLDER, filename)
-    if not os.path.exists(filepath):
-        return jsonify({'error': 'Файл не найден', 'path': filepath}), 404
-    return send_file(filepath, as_attachment=True)
+    return jsonify({'status': 'успешно', 'received': data})
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
