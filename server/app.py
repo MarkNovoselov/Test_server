18d94497f5cff8cd1d441cdf17b7e625692b431f
MarkNovoselov <167557089+MarkNovoselov@users.noreply.github.com>
2025-07-05 21:26:17 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -3,18 +3,12 @@ import os
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'downloads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/') # проверка связи
 def home():
-    return "Сервер работает!"
+    return {'respone':200}
 
-@app.route('/json', methods=['POST']) # отправка json
-def json_endpoint():
-    data = request.get_json()
-    if not data:
-        return jsonify({'error': 'Нет JSON'}), 400
-    return jsonify({'status': 'успешно', 'received': data})
+#@app.route('/') # register
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
